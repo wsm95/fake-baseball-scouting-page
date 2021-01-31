f2680201fb8c549c81a4407b070592290ac68cbc
wsm95 <wsmcrae93@gmail.com>
2021-01-31 14:46:33 -0600

    add basename to router

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -5,20 +5,6 @@ import { Routes } from "../routes/Routes";
 export const App: React.FC = () => {
   return (
     <div className="App">
-      <nav>
-        <ul>
-          <li>
-            <a href="/">Default</a>
-          </li>
-          <li>
-            <a href="/game">Game Page</a>
-          </li>
-          <li>
-            <a href="/player">Player Page</a>
-          </li>
-        </ul>
-      </nav>
-
       <Routes />
     </div>
   );
--- src/components/routes/Routes.tsx
+++ src/components/routes/Routes.tsx
@@ -3,25 +3,38 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect
+  Redirect,
+  Link
 } from "react-router-dom";
 import { GamePage } from "../pages/gamePage/gamePage";
 import { PlayerPage } from "../pages/playerPage/playerPage";
 
 export const Routes: React.FC = () => {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Redirect exact from="/" to="/game" />
-          <Route path="/game">
-            <GamePage />
-          </Route>
-          <Route path="/player">
-            <PlayerPage />
-          </Route>
-        </Switch>
-      </div>
+    <Router basename="/fake-baseball-scouting-page/">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Default</Link>
+          </li>
+          <li>
+            <Link to="/game">Game Page</Link>
+          </li>
+          <li>
+            <Link to="/player">Player Page</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Redirect exact from="/" to="/game" />
+        <Route path="/game">
+          <GamePage />
+        </Route>
+        <Route path="/player">
+          <PlayerPage />
+        </Route>
+      </Switch>
     </Router>
   );
 };
