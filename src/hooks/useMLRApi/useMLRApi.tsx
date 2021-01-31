46a6cf10ae39d7ad54f7aeec503eed832cb47865
wsm95 <wsmcrae93@gmail.com>
2021-01-31 16:50:11 -0600

    remove unused variable

--- src/hooks/useMLRApi/useMLRApi.tsx
+++ src/hooks/useMLRApi/useMLRApi.tsx
@@ -1,4 +1,4 @@
-import useFetch, { UseFetch } from "use-http";
+import useFetch from "use-http";
 
 export const useMLRApi = <T,>(apiVerson: string = "v1") => {
   return useFetch<T>(`https://redditball.com/api/${apiVerson}`);
