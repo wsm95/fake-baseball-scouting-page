63b683144d3fcc8ef47c93cd77087d542d75d250
wsm95 <wsmcrae93@gmail.com>
2021-02-17 21:14:21 -0600

    add player name to player page

---
+++ src/components/playerStats/playerStats.tsx
@@ -0,0 +1,17 @@
+import { FlexboxGrid } from "rsuite";
+import { Player } from "../../interfaces/player";
+
+interface PlayerStatsProps {
+  player: Player;
+  statType: "pitching" | "batting";
+}
+
+export const PlayerStats = (props: PlayerStatsProps) => {
+  const { player, statType } = props;
+  console.log(player);
+  return (
+    <div style={{ width: "100%", textAlign: "center" }}>
+      <h3>{`${player.firstName} ${player.lastName}`}</h3>
+    </div>
+  );
+};
